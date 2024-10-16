a4b093a7c0ccaae8a09b8cef4996d0f6a6db5a77
abhaykumar-0285 <abhay.kr2803@gmail.com>
2024-10-16 14:17:07 +0530

    update new scroll up down button

--- script.js
+++ script.js
@@ -95,5 +95,32 @@ newsletterForm.addEventListener('submit', (event) => {
     // Clear previous error message
     newsletterErrorMessage.textContent = '';
 
-    if (!emailPattern.test(email)) {
-        newsletterErrorMessage.textContent = 'Please enter
+    if (!emailPattern.test(email)) 
+    {
+        newsletterErrorMessage.textContent = 'Please enter'
+    }});
+
+
+
+
+
+    // Get the button
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+// Show the button when the user scrolls down 300px from the top
+window.onscroll = function() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+};
+
+// When the user clicks the button, scroll to the top of the document
+scrollTopBtn.addEventListener("click", function() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth' // Smooth scrolling effect
+  });
+});
+
